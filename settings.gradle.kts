7113e9bd9e01b63e4d2124ccc180e89ceb1c525a
Johoseong <whghtjd320@naver.com>
2024-08-24 17:51:10 +0900

    feat: 헥사고날 아키텍처에 따라 모듈 생성
    - 참고: Github buckpal project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "ai-world"
+
+include("adapter:adapter-in")
+include("adapter:adapter-out")
+include("bootstrap")
+include("application:port-in")
+include("application:port-out")
+include("application:domain")
